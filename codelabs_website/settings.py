16b234943f2c13e1a0466644d96550ba726023c2
juan porras <jporrase@nd.edu>
2023-11-29 12:52:00 -0500

    changes

--- codelabs_website/settings.py
+++ codelabs_website/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/staticfiles/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/website")
 
 
 
