b4d93d1175c68c28519d986a370112a3b40e53a0
DiegoCruz17 <degocruzruiz@gmail.com>
2023-12-28 16:40:03 -0500

    more pages

--- website/urls.py
+++ website/urls.py
@@ -5,6 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.index, name='home'),
+    path('about-us/',views.about, name='about'),
+    path('product/',views.product, name='product'),
+    path('contact/',views.contact, name='contact'),
 ]
 
 if settings.DEBUG:
--- website/views.py
+++ website/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'website/index.html', {})
     
+def product(request):
+    return render(request, 'website/product.html', {})
+    
+def contact(request):
+    return render(request, 'website/contact.html', {})
+    
+def about(request):
+    return render(request, 'website/about.html', {})
+    
