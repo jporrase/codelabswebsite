80ecd280a0b13bda9557f697a6a6e269a029caef
DiegoCruz17 <degocruzruiz@gmail.com>
2023-12-29 22:40:59 -0500

    minor mobile fixes

--- website/static/website/js/scripts.js
+++ website/static/website/js/scripts.js
@@ -1,5 +1,38 @@
+let slideIndex = 0;
+
+function showSlides() {
+    let i;
+    const slides = document.getElementsByClassName("slide");
+    const dots = document.getElementsByClassName("dot");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "flex";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 10000); // Change slide every 2 seconds (adjust as needed)
+}
+
+// Function to set the current slide
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
 // Wait for the DOM to fully load before running the scripts
 document.addEventListener('DOMContentLoaded', (event) => {
+
+    showSlides();
     // Smooth scroll for navigation links
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
@@ -11,73 +44,4 @@ document.addEventListener('DOMContentLoaded', (event) => {
         });
     });
 
-    // Responsive navigation toggle
-    const navToggle = document.createElement('button');
-    navToggle.innerText = 'Menu';
-    navToggle.classList.add('nav-toggle');
-    const nav = document.querySelector('nav');
-    nav.parentNode.insertBefore(navToggle, nav);
-
-    navToggle.addEventListener('click', () => {
-        nav.classList.toggle('active');
-    });
-
-    // Back to top button functionality
-    const backToTopButton = document.createElement('button');
-
-    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    svg.setAttribute('fill-rule', 'evenodd');
-    svg.setAttribute('clip-rule', 'evenodd');
-    svg.setAttribute('width', '24');
-    svg.setAttribute('height', '24');
-    svg.setAttribute('viewBox', '0 0 24 24');
-    // <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M11 2.206l-6.235 7.528-.765-.645 7.521-9 7.479 9-.764.646-6.236-7.53v21.884h-1v-21.883z"/></svg>
-
-  
-    // Create an SVG path element with custom path data
-    const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    path.setAttribute('d', 'M12 0l8 9h-6v15h-4v-15h-6z');
-
-  
-    // Append the path to the SVG element
-    svg.appendChild(path);
-  
-    // Append the SVG element to the button
-    backToTopButton.appendChild(svg);
-  
-    // backToTopButton.innerText = '↑';
-    backToTopButton.classList.add('back-to-top');
-    backToTopButton.style.display = 'none'; // Hide the button initially
-    document.body.appendChild(backToTopButton);
-
-    backToTopButton.addEventListener('click', () => {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    });
-
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > 500) { // Show back to top button after scrolling down 500px
-            backToTopButton.style.display = 'block';
-        } else {
-            backToTopButton.style.display = 'none';
-        }
-    });
-
-    // Animate testimonials on scroll
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('slide-in');
-            } else {
-                entry.target.classList.remove('slide-in');
-            }
-        });
-    }, {
-        rootMargin: '0px',
-        threshold: 0.1
-    });
-
-    document.querySelectorAll('.testimonial').forEach(testimonial => {
-        observer.observe(testimonial);
-    });
 });
-
